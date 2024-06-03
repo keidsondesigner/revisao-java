e09df25beddc7dce032e38c29347f38778f1d62d
Keidson Roby <keidsondesigner@gmail.com>
2024-06-02 22:18:44 -0400

    feat: :sparkles: Operadores Relacionais

---
+++ src/Aula5OperadorRelacional.java
@@ -0,0 +1,21 @@
+public class Aula5OperadorRelacional {
+  public static void main(String[] args) {
+    // Operadores Relacionais
+    // ==, !=, >, <, >=, <=
+    // sempre retorna um valor booleano.
+
+    boolean result1 = 10 == 10;
+    boolean result2 = 10 != 10;
+    boolean result3 = 10 > 10;
+    boolean result4 = 10 < 10;
+    boolean result5 = 10 >= 10;
+    boolean result6 = 10 <= 10;
+
+    System.out.println(result1);
+    System.out.println(result2);
+    System.out.println(result3);
+    System.out.println(result4);
+    System.out.println(result5);
+    System.out.println(result6);
+  }
+}
