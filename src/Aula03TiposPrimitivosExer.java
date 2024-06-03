905fb1fb181d6a29cae48765690cc654258b629e
Keidson Roby <keidsondesigner@gmail.com>
2024-06-02 21:59:34 -0400

    feat: :sparkles: Exercicio tipos primitivos

---
+++ src/Aula03TiposPrimitivosExer.java
@@ -0,0 +1,12 @@
+public class Aula03TiposPrimitivosExer {
+  public static void main(String[] args) {
+    String nome = "Keidson";
+    String adress = "Av. Brigadeiro Faria Lima";
+    double salary = 2500.95;
+
+    System.out.println(
+      "Eu " + nome + " moro no endereco " + adress + 
+      "\n confirmo que recebi o salario de " + salary
+    );
+  }
+}
