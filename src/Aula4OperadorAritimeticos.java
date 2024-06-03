71269841706c1a883685c0ec740c0fba515b1d9e
Keidson Roby <keidsondesigner@gmail.com>
2024-06-02 22:16:04 -0400

    feat: :sparkles: Operadores Aritiméticos

---
+++ src/Aula4OperadorAritimeticos.java
@@ -0,0 +1,24 @@
+public class Aula4OperadorAritimeticos {
+
+  public static void main(String[] args) {
+    // Operadores Aritimeticos
+    // +,  -,  *,  /,  %
+
+    int num1 = 10;
+    int num2 = 20;
+
+    double num3 = 20;
+
+    int result1 = num1 + num2;
+    int result2 = num1 - num2;
+    int result3 = num1 * num2;
+    double result4 = num1 / num3;
+    int result5 = num1 % num2;
+
+    System.out.println(result1);
+    System.out.println(result2);
+    System.out.println(result3);
+    System.out.println(result4);
+    System.out.println(result5);
+  }
+}
