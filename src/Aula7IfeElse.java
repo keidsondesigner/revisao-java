3672647ab1f8d7b49bdd5b23800c361871949f76
Keidson Roby <keidsondesigner@gmail.com>
2024-06-06 20:28:12 -0400

    feat ELSE e IF

---
+++ src/Aula7IfeElse.java
@@ -0,0 +1,13 @@
+public class Aula7IfeElse {
+
+  public static void main(String[] args) {
+
+    int age = 17;
+
+    if (age >= 18) {
+      System.out.println("Maior de idade");
+    } else {
+      System.out.println("Menor de idade");
+    }
+  }
+}
