ef2cc9badfc7a3da7309ba08ed6c35fa88fd5c11
keidson designer <keidsondesigner@gmail.com>
2024-06-08 13:47:51 -0400

    feat: métodos em Class

---
+++ src/domain/Calculadora.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class Calculadora {
+
+  public int metodoSomar(int valor1, int valor2) {
+    return valor1 + valor2;
+  }
+  
+}
---
+++ src/test/CalculadoraTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import domain.Calculadora;
+
+public class CalculadoraTest {
+
+  public static void main(String[] args) {
+    Calculadora calculadora = new Calculadora();
+
+    System.out.println(calculadora.metodoSomar(10, 20));
+  }
+}
