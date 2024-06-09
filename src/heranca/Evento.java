e8c72a080502a4ccfa5843e85f6212d4124c1f64
Keidson Roby <keidsondesigner@gmail.com>
2024-06-09 14:58:12 -0400

    feat(Heranca): criado Class de Eventos e Seminarios para Associação

---
+++ src/heranca/Evento.java
@@ -0,0 +1,20 @@
+package heranca;
+
+
+public class Evento {
+  private String titulo;
+
+  public Evento() { }
+
+  public Evento(String titulo) {
+    this.titulo = titulo;
+  }
+
+  public String getTitulo() {
+    return titulo;
+  }
+
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
+}
---
+++ src/heranca/LocalEvento.java
@@ -0,0 +1,20 @@
+package heranca;
+
+public class LocalEvento {
+  private String endereco;
+
+  public LocalEvento() { }
+
+  public LocalEvento(String endereco) {
+    this.endereco = endereco;
+  }
+
+  public String getEndereco() {
+    return endereco;
+  }
+
+  public void setEndereco(String endereco) {
+    this.endereco = endereco;
+  }
+  
+}
