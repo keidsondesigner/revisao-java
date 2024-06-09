aee3caf622a8f45ef98247457af6c72f4c2325b4
Keidson Roby <keidsondesigner@gmail.com>
2024-06-09 15:15:45 -0400

    feat(Heranca): um Professor poderá ministrar vários Eventos;

--- src/heranca/Professor.java
+++ src/heranca/Professor.java
@@ -3,12 +3,19 @@ package heranca;
 public class Professor {
   private String nome;
   private String especialidade;
+  private Evento[] eventos;
 
   public Professor() { }
 
   public Professor(String nome, String especialidade) {
     this.nome = nome;
     this.especialidade = especialidade;
+  }
+
+  public Professor(String nome, String especialidade, Evento[] eventos) {
+    this.nome = nome;
+    this.especialidade = especialidade;
+    this.eventos = eventos;
   }
 
   public String getNome() {
@@ -25,5 +32,13 @@ public class Professor {
 
   public void setEspecialidade(String especialidade) {
     this.especialidade = especialidade;
+  }
+
+  public Evento[] getEventos() {
+    return eventos;
+  }
+
+  public void setEventos(Evento[] eventos) {
+    this.eventos = eventos;
   }
 }
