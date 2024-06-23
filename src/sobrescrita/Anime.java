1747292c5cd8fc21b5e7696abc37c8b535c64236
Keidson Roby <keidsondesigner@gmail.com>
2024-06-22 20:51:27 -0400

    criado Class anime

---
+++ src/sobrescrita/Anime.java
@@ -0,0 +1,17 @@
+package sobrescrita;
+
+public class Anime {
+  private String nome;
+
+  public Anime(String nome) {
+    this.nome = nome;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+}
---
+++ src/test/AnimeTeste.java
@@ -0,0 +1,11 @@
+package test;
+
+import sobrescrita.Anime;
+
+public class AnimeTeste {
+  public static void main(String[] args) {
+
+    Anime anime = new Anime("One Piece");
+    System.out.println(anime.getNome());
+  }
+}
